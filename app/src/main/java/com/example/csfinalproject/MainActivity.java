e047a9110bae6425cfe37d18f06124ff7f357b33
duncankoblas <dkoblas2@illinois.edu>
2019-04-20 16:38:05 -0500

    test3

--- app/src/main/java/com/example/csfinalproject/MainActivity.java
+++ app/src/main/java/com/example/csfinalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.csfinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SmsManager smsManager = SmsManager.getDefault();
+//        smsManager.sendTextMessage("6508682328", null, "sms message", null, null);
     }
 }

11ab8f082bfb2f0f528b8d0e5b89fbd405e77653
Shivani <ingle3@illinois.edu>
2019-04-22 15:08:22 -0500

    CS 125

--- app/src/main/java/com/example/csfinalproject/MainActivity.java
+++ app/src/main/java/com/example/csfinalproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.csfinalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.editText1);
+        editText.setOnClickListener(v -> {
+            editText.setText("");
+        });
+
 
 //        SmsManager smsManager = SmsManager.getDefault();
 //        smsManager.sendTextMessage("6508682328", null, "sms message", null, null);

5a1f299911f2c57d26366e9c3687e6cc6576c0f1
duncankoblas <dkoblas2@illinois.edu>
2019-04-22 15:33:48 -0500

    test5

--- app/src/main/java/com/example/csfinalproject/MainActivity.java
+++ app/src/main/java/com/example/csfinalproject/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.csfinalproject;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,30 @@ public class MainActivity extends AppCompatActivity {
         editText.setOnClickListener(v -> {
             editText.setText("");
         });
+
+        final Button getContactButton = findViewById(R.id.getContact);
+        getContactButton.setOnClickListener(v -> {
+            Cursor contacts = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            String aNameFromContacts[] = new String[contacts.getCount()];
+            String aNumberFromContacts[] = new String[contacts.getCount()];
+            int i = 0;
+
+            int nameFieldColumnIndex = contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+            int numberFieldColumnIndex = contacts.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            while(contacts.moveToNext()) {
+
+                String contactName = contacts.getString(nameFieldColumnIndex);
+                aNameFromContacts[i] =    contactName ;
+
+                String number = contacts.getString(numberFieldColumnIndex);
+                aNumberFromContacts[i] =    number ;
+                i++;
+            }
+
+            contacts.close();
+        });
+
 
 
 //       smsManager.sendTextMessage("6508682328", null, "sms message", null, null);

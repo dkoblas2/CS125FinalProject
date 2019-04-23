30344e9af974c0855f3aa394f25db3595af1a366
duncankoblas <dkoblas2@illinois.edu>
2019-04-23 09:49:47 -0500

    test5

--- app/src/main/java/com/example/csfinalproject/MainActivity.java
+++ app/src/main/java/com/example/csfinalproject/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.csfinalproject;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,31 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        findViewById(R.id.ContactList).setOnClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+
+
+                Cursor item = (Cursor) parent.getItemAtPosition(position);
+                item.getString(position);
+
+                TextView contactName = findViewById(R.id.contact);
+                contactName.setText(item.getString(position));
+
+//                ListEntry entry= (ListEntry) parent.getAdapter().getItem(position);
+//                Intent intent = new Intent(MainActivity.this, SendMessage.class);
+//                String message = entry.getMessage();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+//                startActivity(intent);
+            }
+        });
+
+
+
+
 
 //       smsManager.sendTextMessage("6508682328", null, "sms message", null, null);
     }
@@ -51,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
         SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, c, from, to);
         contactList.setAdapter(simpleCursorAdapter);
         contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+
+    public void onActivityResult() {
 
     }
 }

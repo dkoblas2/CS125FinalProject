d150ee7b3a58e0973eecbfc8e9cc5fc09d10c6a6
Shivani <ingle3@illinois.edu>
2019-04-28 22:22:31 -0500

    CS 125

--- app/src/main/java/com/example/csfinalproject/MainActivity.java
+++ app/src/main/java/com/example/csfinalproject/MainActivity.java
@@ -5,74 +5,67 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
-    EditText no,msg;            //Added
-    Button sendSms;             //Added
+    //Added
+    Button sendB;
+    EditText phone, message;
+
+    //Ends
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendB = (Button) findViewById(R.id.send);
+        phone = (EditText) findViewById(R.id.txtPhone);
+        message = (EditText) findViewById(R.id.txtMessage);
+        sendB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendMessage();
+            }
+
+        });
+    }
+    public void sendMessage() {
+        String number = phone.getText().toString().trim();
+        String mess = message.getText().toString().trim();
+        if (number == null || number.equals("") || mess == null || mess.equals("")) {
+            Toast.makeText(this,"field can't be empty",Toast.LENGTH_LONG).show();
+        } else {
+            if (TextUtils.isDigitsOnly(number)) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(number, null, mess, null, null);
+                Toast.makeText(this, "message sent successfully!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,"please enter integer only",Toast.LENGTH_LONG).show();
+        }
+    }
+
+        //Added
+
 //        final EditText editText = findViewById(R.id.editText1);
 //        editText.setOnClickListener(v -> {
 //            editText.setText("");
 //        });
 
-        findViewById(R.id.clear).setOnClickListener(v -> {
-
-        });
+//        findViewById(R.id.clear).setOnClickListener(v -> {
+//
+//        });
 
         final Button getContactButton = findViewById(R.id.getContact);
         final ListView contactList = findViewById(R.id.ContactList);
         getContactButton.setOnClickListener(v -> {
             getContact(contactList);
         });
-
-
-//        findViewById(R.id.ContactList).setOnClickListener(new AdapterView.OnItemClickListener() {
-//
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position,
-//                                    long id) {
-//
-//
-//
-//                Cursor item = (Cursor) parent.getItemAtPosition(position);
-//                item.getString(position);
-//
-//                TextView contactName = findViewById(R.id.contact);
-//                contactName.setText(item.getString(position));
-//
-////                ListEntry entry= (ListEntry) parent.getAdapter().getItem(position);
-////                Intent intent = new Intent(MainActivity.this, SendMessage.class);
-////                String message = entry.getMessage();
-////                intent.putExtra(EXTRA_MESSAGE, message);
-////                startActivity(intent);
-//            }
-//        });
-        //no=(EditText)findViewById(R.id.num);   // ???
-        msg=(EditText)findViewById(R.id.message);
-        sendSms=(Button)findViewById(R.id.send);
-        sendSms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg) {
-                sendMsg();
-            }
-
-            private void sendMsg() {
-                String number=no.getText().toString();
-                String message=msg.getText().toString();
-                SmsManager manager=SmsManager.getDefault();
-                manager.sendTextMessage(number, null, message, null,null);
-                //Toast.makeText(getApplicationContext(),"send successfully". Toast.LENGTH LONG           //
-            }
-        });
-
-}
+    }
 
     public void getContact(View v) {
         final ListView contactList = findViewById(R.id.ContactList);
@@ -87,9 +80,5 @@ public class MainActivity extends AppCompatActivity {
         SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, c, from, to);
         contactList.setAdapter(simpleCursorAdapter);
         contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-    }
-
-    public void onActivityResult() {
-
     }
 }
